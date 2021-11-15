6b22b42ab8b3d41ded0b747e35462e0a0ac99ea9
Stanley Luong <slluong@uw.edu>
2021-11-15 02:09:56 -0800

    oauth

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js'
+import userRoutes from './routes/users.js'
 
 const app = express();
 dotenv.config();
@@ -11,7 +12,9 @@ dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
 app.use('/posts',postRoutes)
+app.use('/user', userRoutes)
 
 app.get('/', (req, res) => {
     res.send('Hello to memories API')
