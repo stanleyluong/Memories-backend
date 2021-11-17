2b9c606300d412ad17b3892043574449f414e7df
Stanley Luong <slluong@uw.edu>
2021-11-17 03:17:39 -0800

    pagination p4

--- models/postMessage.js
+++ models/postMessage.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 
 const postSchema = mongoose.Schema({
     title: String,
@@ -15,8 +15,8 @@ const postSchema = mongoose.Schema({
         type: Date,
         default: new Date()
     }
-});
+})
 
 const PostMessage = mongoose.model('PostMessage', postSchema)
 
-export default PostMessage;
+export default PostMessage
