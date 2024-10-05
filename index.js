f7c845fb43136d5c46444b7b6d92d3072a81b322
Stanley Luong <slluong@uw.edu>
2024-10-05 16:57:50 -0700

    fixed cors options

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ dotenv.config()
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 const corsOptions = {
-  origin: ['http://localhost:5000', 'https://www.stanleyluong.com'],
+  origin: ['http://localhost:3000', 'https://www.stanleyluong.com', 'https://stanleyluong.com/Memories'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
