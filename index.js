dfc2e56f53cdb8151e49dbab4cfa5903bab0157a
Stanley Luong <slluong@uw.edu>
2024-10-06 12:14:56 -0700

    remove comments, run prettier

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import postRoutes from './routes/posts.js'
-import userRoutes from './routes/users.js'
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
 
-mongoose.set('strictQuery', true);
+mongoose.set("strictQuery", true);
 
-const app = express()
-dotenv.config()
+const app = express();
+dotenv.config();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }))
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://www.stanleyluong.com', 'https://stanleyluong.com/Memories'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: [
+    "http://localhost:3000",
+    "https://www.stanleyluong.com",
+    "https://stanleyluong.com/Memories",
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
 app.use(cors(corsOptions));
 
-app.use('/posts',postRoutes)
-app.use('/user', userRoutes)
+app.use("/posts", postRoutes);
+app.use("/user", userRoutes);
 
-app.get('/', (req, res) => {
-    res.send('welcome to the Memories API')
-})
+app.get("/", (req, res) => {
+  res.send("welcome to the Memories API");
+});
 
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5001;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch((error) => console.log(error.message))
-
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)),
+  )
+  .catch((error) => console.log(error.message));
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,33 +1,33 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const auth = async (req, res, next) => {
-    console.log('auth middleware');
-    try {
-        const authHeader = req.headers.authorization;
-        console.log('authHeader', authHeader);
-        if (!authHeader) {
-            throw new Error("Authorization header missing");
-        }
-        const token = authHeader.split(" ")[1];
-        if (!token) {
-            throw new Error("Token missing");
-        }
-
-        const isCustomAuth = token.length < 500;
-        let decodedData;
-
-        if (token && isCustomAuth) {
-            decodedData = jwt.verify(token, 'test');
-            req.userId = decodedData?.id;
-        } else {
-            decodedData = jwt.decode(token);
-            req.userId = decodedData?.sub;
-        }
-        next();
-    } catch (error) {
-        console.log(error);
-        res.status(401).json({ message: "Authentication failed", error: error.message });
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      throw new Error("Authorization header missing");
     }
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+      throw new Error("Token missing");
+    }
+
+    const isCustomAuth = token.length < 500;
+    let decodedData;
+
+    if (token && isCustomAuth) {
+      decodedData = jwt.verify(token, "test");
+      req.userId = decodedData?.id;
+    } else {
+      decodedData = jwt.decode(token);
+      req.userId = decodedData?.sub;
+    }
+    next();
+  } catch (error) {
+    console.log(error);
+    res
+      .status(401)
+      .json({ message: "Authentication failed", error: error.message });
+  }
 };
 
 export default auth;
